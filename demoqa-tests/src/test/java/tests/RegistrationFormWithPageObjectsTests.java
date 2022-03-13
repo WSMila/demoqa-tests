99c7ecabe09f7a0643a488b488c5b93c50330fcb
gladkov@ukr.net <1Ama2deus#>
2022-03-13 16:20:08 +0200

    added changes by less 3

--- demoqa-tests/src/test/java/tests/RegistrationFormWithPageObjectsTests.java
+++ demoqa-tests/src/test/java/tests/RegistrationFormWithPageObjectsTests.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -22,8 +23,9 @@ public class RegistrationFormWithPageObjectsTests {
 
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        $("#firstName").setValue("Yuriy");
-        $("#lastName").setValue("Gladkov");
+
+        new RegistrationPage().setFirstName("Yuriy");
+        new RegistrationPage().setLastName("Gladkov");
         $("#userEmail").setValue("gladkov@ua.net");
         $("#genterWrapper").$(byText("Other")).click();
         $("#userNumber").setValue("+380963355447");
@@ -33,7 +35,7 @@ public class RegistrationFormWithPageObjectsTests {
         $("[aria-label$='June 12th, 1980']").click();
         $("#subjectsInput").setValue("Math").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("/home/wsmila/IdiaProjects/demoqa-tests/demoqa-tests/1.png");
+        $("#uploadPicture").uploadFromClasspath("/home/wsmila/IdiaProjects/demoqa-tests/demoqa-tests/src/test/resources/1.jpg");
         $("#currentAddres").setValue("Some address 1");
         $("#state").scrollTo().click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
@@ -42,8 +44,9 @@ public class RegistrationFormWithPageObjectsTests {
         $("#submit").click();
 
         $("#example-model-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").$(byText("Student Name"))
-                .parent().shouldHave(text("Yuriy Gladkov"));
+        new RegistrationPage().checkForm("Student Name", "Yuriy Gladkov");
+        new RegistrationPage().checkForm("Student Email", "gladkov@ua.net");
+        new RegistrationPage().checkForm("Gender", "Other");
 
     }
 
